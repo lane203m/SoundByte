e32ca6da6e2bd69ea796440e1a7f92f6632bb320
lane203m <mlane0921@hotmail.com>
2020-12-29 10:29:27 -0600

    Communication between JS & Python.
    Reading id3 info in python, sending to JS

---
+++ Soundbyte-App/Packages/Initialization/script.js
@@ -0,0 +1,14 @@
+const {PythonShell} = require('python-shell');
+
+let pyshell = new PythonShell('script.py');
+
+pyshell.on('message', function(message) {
+  console.log(message);
+})
+
+pyshell.end(function (err) {
+  if (err){
+  throw err;
+  };
+  console.log('finished');
+});
---
+++ Soundbyte-App/Packages/Initialization/script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import sys
+print("Command executed from Python script")
+sys.stdout.flush()
+
+
+# In[ ]:
+
+
+
+
