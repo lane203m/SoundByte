6b87c4b661d357c7d029d4012332261f9344fd37
lane203m <mlane0921@hotmail.com>
2021-01-10 23:42:11 -0600

    adding more ts, first time compiling. Adding tsc file.

---
+++ Soundbyte-App/Packages/Types/LibraryData.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LibraryData = void 0;
+var LibraryData = /** @class */ (function () {
+    function LibraryData() {
+        //this.songs = (<any>data).songs;
+    }
+    LibraryData.prototype.getSong = function (n) {
+        return this.songs[n];
+    };
+    return LibraryData;
+}());
+exports.LibraryData = LibraryData;
+//# sourceMappingURL=LibraryData.js.map
--- Soundbyte-App/Packages/Types/LibraryData.ts
+++ Soundbyte-App/Packages/Types/LibraryData.ts
@@ -1,12 +1,12 @@
 import {Feature} from "./Feature"
 import {Song} from "./Song"
-import data from "../Libraries/songLibrary/library.json"
+import * as data from "../Libraries/songLibrary/library.json"
 
 export class LibraryData {
   songs: Song[];
 
   constructor(){
-    this.songs = (<any>data).songs;
+    //this.songs = (<any>data).songs;
   }
 
   getSong(n: number){
