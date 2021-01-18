432db90baa0597653a14e42bcb00b0ac19b83d1d
lane203m <mlane0921@hotmail.com>
2021-01-18 04:40:33 -0600

    editing index. Improving suggestion ui

--- Soundbyte-App/src/songMenu/songMenu.js
+++ Soundbyte-App/src/songMenu/songMenu.js
@@ -7,38 +7,66 @@ function showSongs() {
     var songLibrary = new LibraryData();
     console.log(songLibrary);
     for (var i = 0; i<songLibrary.songs.length; i++){
-        var node = document.createElement("BUTTON");
-        
-        var div = document.createElement("div");
-        div.classList.add("songContainer");
-        makeHeaderElement(div, "", songLibrary.songs[i].songName);
-        makeHeaderElement(div, "By: ", songLibrary.songs[i].author);
-        node.appendChild(div);
+      var listingDiv = document.createElement("div");
+      listingDiv.classList.add("listing");
 
-        var div = document.createElement("div");
-        div.classList.add("songContainer");
-        makeBodyElement(div, "Bpm: ", songLibrary.songs[i].features.bpm);
-        makeBodyElement(div, "Key: ", songLibrary.songs[i].features.key);
-        makeBodyElement(div, "Scale: ", songLibrary.songs[i].features.scale);
-        node.appendChild(div);
 
-        document.getElementById("songList").appendChild(node);
+      var node = document.createElement("BUTTON");  
+      var dataDiv = document.createElement("div");
+      dataDiv.classList.add("metaDataDiv");
+          var div = document.createElement("div");
+          div.classList.add("songNameContainer");
+          makeHeaderElement(div, ""+ songLibrary.songs[i].songName);
+          dataDiv.appendChild(div);
+
+
+
+          var div = document.createElement("div");
+          div.classList.add("songBPMContainer");
+          makeBodyElement(div, "Bpm: "+ songLibrary.songs[i].features.bpm);
+          dataDiv.appendChild(div);
+
+          var div = document.createElement("div");
+          div.classList.add("songAuthorContainer");
+          makeHeaderElement(div, "By: "+ songLibrary.songs[i].author);
+          dataDiv.appendChild(div);
+
+          var div = document.createElement("div");
+          div.classList.add("songKeyContainer");
+          makeBodyElement(div, "Key: "+ songLibrary.songs[i].features.key + " " + songLibrary.songs[i].features.scale);
+          dataDiv.appendChild(div);
+
+
+          node.appendChild(dataDiv);
+
+          listingDiv.appendChild(node);
+
+
+
+          var node2 = document.createElement("BUTTON");
+          node2.classList.add("PlayButton");
+          var div = document.createElement("div");
+          div.innerHTML = '&#9658'
+          node2.appendChild(div);
+          listingDiv.appendChild(node2);
+
+          document.getElementById("songList").appendChild(listingDiv);
     }
     
   }
 
-  function makeHeaderElement(div, type, value) {
+  function makeHeaderElement(div, value) {
     var h1 = document.createElement("h1");
     h1.classList.add("songContainer");
     div.appendChild(h1);
-    var textnode = document.createTextNode(type+value);
+    var textnode = document.createTextNode(value);
     div.appendChild(textnode);
   }
 
-  function makeBodyElement(div, type, value) {
+  function makeBodyElement(div, value) {
     var p = document.createElement("p");
     p.classList.add("songContainer");
     div.appendChild(p);
-    var textnode = document.createTextNode(type+value);
+    var textnode = document.createTextNode(value);
     div.appendChild(textnode);
   }
