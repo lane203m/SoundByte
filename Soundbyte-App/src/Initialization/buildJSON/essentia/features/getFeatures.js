6a32a1e9d679b48d2fb7c4e6c2dc7a6b4ad243ff
lane203m <mlane0921@hotmail.com>
2021-03-03 02:58:29 -0600

    Refactoring. Making libraryBuilder. Comments
    removing unnecessary files, code, and comments.
    
    commented code. added authorship
    
    reorganized libraryBuilder. Removed redundancies.

--- Soundbyte-App/src/Initialization/buildJSON/essentia/features/getFeatures.js
+++
@@ -1,8 +0,0 @@
-import PromiseWorker from 'promise-worker';
-import Worker from '../../workers/primes/node_modules/worker-loader!./essentiaFeatureWorker';
-const worker = new Worker();
-const promiseWorker = new PromiseWorker(worker);
-const getPrimes = (amount) => promiseWorker.postMessage({
-  type: 'getPrimesMessage', amount
-});
-export default { getPrimes }
--- Soundbyte-App/src/Initialization/buildJSON/workers/primes/essentiaWorker.js
+++ Soundbyte-App/src/Initialization/buildJSON/workers/primes/essentiaWorker.js
@@ -1,71 +1,26 @@
-//var essentia = require('../../essentia/essentia-wasm.web.js');
-//var EssentiaWASM = require('../../essentia/essentia.js-core.js');
-
-//importScripts("../../essentia/essentia-wasm.web.js");
-//importScripts("../../essentia/essentia.js-core.js");
-//let essentia = new Essentia(EssentiaWASM);
-
-//console.log(essentia.version);
-//import { expose } from "comlink";
-//import Essentia from '../../essentia/essentia.js-core.js';
-//import {EssentiaWASM} from '../../essentia/essentia-wasm.web.js';
-//const essentia = new Essentia(EssentiaWASM);
-//import {test} from './test.js';
-/*var i;
-*/
+//Essentia worker
+//Mason Lane
+//2021-03-03
+//since we use electron, workers may import node modules
+//we use the essentiaJS node module to extract features
+//from a given decoded audioData
 
 const essentia = require('essentia.js');
 
-addEventListener('message', e => {
-  console.log(e.data);
-  console.log(essentia.version);
-  let audio = e.data.channelData[0];
-  let inputSignalVector = essentia.arrayToVector(audio);
-  let key = essentia.KeyExtractor(inputSignalVector);
-  let bpm = essentia.RhythmExtractor(inputSignalVector);
-  
 
+addEventListener('message', e => {
+  let audio = e.data.channelData[0];                      //make essentia readable (js does not have vectors, 
+  let inputSignalVector = essentia.arrayToVector(audio);  //but c++ does. essentia needs vector)                                                     
+
+  let key = essentia.KeyExtractor(inputSignalVector);     
+  let bpm = essentia.RhythmExtractor(inputSignalVector);
+
+  //our object (same format as feature object)
   let data = {
       bpm : bpm.bpm, 
       key : key.key,
       scale : key.scale
     }
-  console.log("working");
-  console.log(data);
 
   postMessage(data);
-
 });
-/*
-function timedCount() {
-
-    //var essentia = new Essentia(EssentiaWASM);
-    console.log(essentia.verison);
-  i = {
-      bpm: 25,
-      key: "A",
-      scale: "Major"
-  }
-  console.log(i);
-  //setTimeout(() => {  move("../../index.html"); }, 4000);
-  postMessage(i);
-  //setTimeout("timedCount()",500);
-}
-
-timedCount();*/
-/*
-const sum = async (a, b) =>
-  new Promise(async resolve => {
-    essentia = new Essentia(EssentiaWASM);
-
-    resolve((a+b));
-  });
-
-expose(sum);*/
-//import { sayHello } from './greet.js';
-/*addEventListener('message', e => {
-  console.log("workerTest");
-  postMessage("test");
-  
-});*/
-
