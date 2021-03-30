4576c5a8f0673f95c08b5d8deee304d1512bd3c6
lane203m <mlane0921@hotmail.com>
2021-03-30 14:10:24 -0600

    Revert "Merge pull request #45 from lane203m/frontend-mod-brian"
    This reverts commit aa32213e8ef37e5c5e4900c82fde7d0d3cf1c40f, reversing
    changes made to 4741ac614e3c0daa151c0234df5e2869023beb8b.

--- Soundbyte-App/src/SavedResults/index.js
+++ Soundbyte-App/src/SavedResults/index.js
@@ -1,161 +1,19 @@
 const fs = require('electron').remote.require('fs');
 const path = require('path');
 const contentTarget = document.querySelector(".item-wraper");
-const resultsPath = path.resolve("./Libraries/savedResults/libraryResults.json");
-let libraryResults = JSON.parse(fs.readFileSync(resultsPath));
+const resultTarget = document.querySelector(".item-title.item-library");
 
-//songLength helper function
-function convertMinSec(miliSec) {
-  if(miliSec % 60 < 10){
-    return (Math.floor(miliSec/60) + ":0" +Math.floor(miliSec % 60));
-  }
-  else{
-    return (Math.floor(miliSec/60) + ":" +Math.floor(miliSec % 60));
-  }  
-}
 
-// Playback functions by Bian on Feb 24, 2021
-// Change the player status each
-const changeState = (state) => {
-  if(state.getAttribute("data-isPlay") == 0) {
-    state.setAttribute("data-isPlay", 1);
-  } else {
-    state.setAttribute("data-isPlay", 0);
-  }
-}
-
-const addPlayback = (target) => {
-
-  const playerTarget = document.querySelector(".player");
-  let player = playback.cloneNode(true);
-  let playerUrl = "";
-  
-  // console.log(player.style.visibility);
-  target.childNodes.forEach(childNode => {    
-    childNode.firstChild.addEventListener('click', () => {
-      // console.log(childNode.getAttribute("data-isPlay"));
-
-      // other play buttons change to stop
-      target.childNodes.forEach(subChild => {        
-        subChild.firstChild.src = "../img/play-button.png";
-        if(subChild != childNode) subChild.setAttribute("data-isPlay", 0);
-      });
-      
-      changeState(childNode);
-
-      if(childNode.getAttribute("data-isPlay") == 0) {
-        childNode.firstChild.src = "../img/play-button.png";
-        childNode.nextSibling.style.visibility='hidden';
-        audio.pause();
-      } else {
-        audio.pause();
-        // console.log(songPath + childNode.getAttribute("data-filename"));
-        childNode.firstChild.src = "../img/stop-button.png";
-        childNode.parentNode.insertBefore(player, childNode.nextSibling);
-        childNode.nextSibling.style.visibility='visible';
-        
-        playerUrl = songPath + childNode.getAttribute("data-filename");
-
-        // console.log(playerUrl);
-        audio = new Audio(playerUrl);
-        audio.play();
-      }
-
-    });
-  });
-}
-// Playback functions: End
-const deleteJson = (library, index) => {
-  for (let i = 0; i < library.length; i++) {
-    if(index == i) {
-      library.splice(i, 1);
-    }
-  }
-}
-
-const deleteLibrary = () => {
-  const target = document.querySelector(".item-wraper");
-  let checked = 0;
-
-  target.childNodes.forEach((item) => {
-    if(item.childNodes[3].firstChild.checked) {
-      checked ++;
-    }
-  });
-
-  if( checked > 0) {
-    for(let i = 0; i < target.childNodes.length; i++) {
-      if(target.childNodes[i].childNodes[3].firstChild.checked) {
-        target.childNodes[i].remove();
-        deleteJson(libraryResults.songs, i);
-        // libraryResults.songs.remove(i);
-        // removeFromLibrary(item)
-      }
-    }
-    console.log(libraryResults);
-    fs.writeFileSync(resultsPath, JSON.stringify(libraryResults));
-
-  } else {
-    alert("No song(s) selected..");
-  }
-  
-}
-
-if (libraryResults.songs.length > 0) {
-  let target = document.querySelector(".buttons");
-  const deleteButton = document.createElement("button");
-  deleteButton.setAttribute("type", "button");
-  deleteButton.setAttribute("onClick", "deleteLibrary()");
-  deleteButton.innerText = "Delete";
-  target.appendChild(deleteButton);
-
-  libraryResults.songs.forEach((i, m, song) => {
-    //console.log(song[m]);
-    let node = document.createElement("div");
-    let img = new Image();  
-    let sname = document.createElement("h3");
-    let detailDiv = document.createElement("div");
-    let detailSpan = document.createElement("h4");
-    let durationDiv = document.createElement("div");
-    let checksDiv = document.createElement("div");
-    let checkInput = document.createElement("input");
-
-    //console.log(song[m].songFile);
-    sname.innerText = song[m].songName;
-    node.setAttribute("data-filename", song[m].songFile);
-    img.src = "../img/play-button.png";
-    detailSpan.innerText = Math.floor(song[m].features.bpm) + " bpm / " + song[m].features.key + " key / " + song[m].features.scale + " scale";
-    durationDiv.innerText = convertMinSec(song[m].songLength);
-
-    node.classList.add("item");
-    node.setAttribute("data-isPlay", 0);
-    detailDiv.classList.add("song-detail");
-    durationDiv.classList.add("duration");    
-    
-    checkInput.setAttribute("type", "checkbox");
-    checkInput.setAttribute("value", m);
-    checkInput.setAttribute("id", m);
-    checkInput.setAttribute("class", "select-for-delete");
-
-    checksDiv.appendChild(checkInput);
-
-    node.appendChild(img);        
-    detailDiv.appendChild(sname);
-    detailDiv.appendChild(detailSpan);
-    
-    node.appendChild(detailDiv);
-    node.appendChild(durationDiv);
-    node.appendChild(checksDiv);
-
-    contentTarget.appendChild(node);
-
-  });
-}
-
-// console.log(libraryResults);
+//initialize the pairing results div
+resultTarget.style.visibility = 'hidden';
 
 
 //Navigation - calibration the url ( the other navigation defined on /src/index.js )
 document.querySelectorAll(".navButton")[1].addEventListener('click', () => {
   location.replace('./index.html');
+});
+
+// configuration nav
+document.querySelector(".setup-button").addEventListener('click', () => {
+  location.replace('../initialization/setSettings/config.html');
 });
--- Soundbyte-App/src/songMenu/songMenu.js
+++ Soundbyte-App/src/songMenu/songMenu.js
@@ -10,11 +10,6 @@ const path = require('path');
 let audio = new Audio();
 let suggestion;
 
-let libraryForSave = {};
-const resultsPath = path.resolve("./Libraries/savedResults/libraryResults.json");
-let libraryResults = JSON.parse(fs.readFileSync(resultsPath));
-// console.log(libraryResults);
-
 // get Initial library and the song path in the library
 // caution for the current directory ./ is equivalent to src/ directory
 const libraryPath = path.resolve(__dirname, "../", "./Initialization/init.json"); 
@@ -38,11 +33,13 @@ if(songPath == undefined || songPath == null || songPath == "" || !fs.existsSync
 
 var songLibrary = new LibraryData();
 
+
 if(songLibrary == undefined || songLibrary == null || songLibrary.songs == null || songLibrary.songs == undefined || songLibrary.songs.length <=0){
   fs.unlinkSync(libraryPath);
   alert("No songs in library");
   location.replace('../index.html');
 }
+
 
 var filteredLibrary = songLibrary;
 var selectedSong = "-1";
@@ -104,7 +101,8 @@ const addPlayback = (target) => {
         
         playerUrl = songPath + childNode.getAttribute("data-filename");
 
-        // console.log(playerUrl);
+        
+        console.log(playerUrl);
         audio = new Audio(playerUrl);
         audio.play();
       }
@@ -114,93 +112,62 @@ const addPlayback = (target) => {
 }
 // Playback functions: End
 
-
 // Added by Brian
-// description for the function: When the list is showing suggestions list, 
-// this function adds save the result(s) button if the user selected the checkbox(es)
-const saveResults = (e) => {
-  const targets = document.querySelectorAll(".save-for-select");
-  let amountCheck = 0
-  targets.forEach((target, index) => {
-    if (target.checked) amountCheck ++;
-    // console.log(target.checked);
-  });
-  
-  // console.log(amountCheck);
-  // console.log(libraryForSave);
-  if (amountCheck == 0){
-    alert("No result(s) is(are) selected");
-    return;
-  } else {
-
-    for (let i = 0; i < targets.length; i++) {     
-      let flag = false; 
-      if(targets[i].checked) {
-        for (let j = 0; j < libraryResults.songs.length; j++) {          
-          if (libraryResults.songs[j].songName == libraryForSave.songs[i].songName) flag = true;
-        }
-
-        // console.log(flag);
-        if(!flag) libraryResults.songs.push(libraryForSave.songs[i]);
-      }
-    }
-
-    fs.writeFileSync(resultsPath, JSON.stringify(libraryResults));
-
-  }
-}
-
 const listupSongs = (library, isSuggestion, inputType) => {   
-    // console.log(library.songs);
+
     // initialize the count for 0 in order to get eligible id for criteria checkbox
     lastCountM = 0;
 
     library.songs.forEach((i, m, song) => {
-      //console.log(song[m]);
-      let node = document.createElement("div");
-      let img = new Image();  
-      let sname = document.createElement("h3");
-      let detailDiv = document.createElement("div");
-      let detailSpan = document.createElement("h4");
-      let durationDiv = document.createElement("div");
-      let checksDiv = document.createElement("div");
-      let checkInput = document.createElement("input");
+    //console.log(song[m]);
+    let node = document.createElement("div");
+    let img = new Image();  
+    let sname = document.createElement("h3");
+    let detailDiv = document.createElement("div");
+    let detailSpan = document.createElement("h4");
+    let durationDiv = document.createElement("div");
+    let scoreDiv = document.createElement("div");
+    let checksDiv = document.createElement("div");
+    let checkInput = document.createElement("input");
 
-      //console.log(song[m].songFile);
-      sname.innerText = song[m].songName;
-      node.setAttribute("data-filename", song[m].songFile);
-      img.src = "../img/play-button.png";
-      detailSpan.innerText = Math.floor(song[m].features.bpm) + " bpm / " + song[m].features.key + " key / " + song[m].features.scale + " scale";
-      durationDiv.innerText = convertMinSec(song[m].songLength);
+    //console.log(song[m].songFile);
+    sname.innerText = song[m].songName;
+    node.setAttribute("data-filename", song[m].songFile);
+    img.src = "../img/play-button.png";
+    detailSpan.innerText = Math.floor(song[m].features.bpm) + " bpm / " + song[m].features.key + " key / " + song[m].features.scale + " scale";
+    durationDiv.innerText = convertMinSec(song[m].songLength);
 
-      node.classList.add("item");
-      node.setAttribute("data-isPlay", 0);
-      detailDiv.classList.add("song-detail");
-      durationDiv.classList.add("duration");
-      
+    scoreDiv.innerText = "Similarity: " + song[m].score.toFixed(3) + "%";
+
+    node.classList.add("item");
+    node.setAttribute("data-isPlay", 0);
+    detailDiv.classList.add("song-detail");
+    durationDiv.classList.add("duration");
+    
+    if(!isSuggestion) {
       checkInput.setAttribute("type", "checkbox");
       checkInput.setAttribute("value", m);
       checkInput.setAttribute("id", m);
-      
-      if(!isSuggestion) {
-        checkInput.setAttribute("onClick", "buttonSelected(this.id)");      
-        lastCountM += 1;
-      } else {
-        checkInput.setAttribute("class", "save-for-select");
-      }
-
+      checkInput.setAttribute("onClick", "buttonSelected(this.id)");
       checksDiv.appendChild(checkInput);
+      lastCountM += 1;
+    }
 
-      node.appendChild(img);    
-          
-      detailDiv.appendChild(sname);
-      detailDiv.appendChild(detailSpan);
-      
-      node.appendChild(detailDiv);
-      node.appendChild(durationDiv);
-      node.appendChild(checksDiv);
+    node.appendChild(img);    
+        
+    detailDiv.appendChild(sname);
+    detailDiv.appendChild(detailSpan);
+    
+    node.appendChild(detailDiv);
+    node.appendChild(durationDiv);
+    if(isSuggestion){
+      node.appendChild(scoreDiv);
+    }
 
-      contentTarget.appendChild(node);
+
+    node.appendChild(checksDiv);
+
+    contentTarget.appendChild(node);
     });
 
     if (lastCountM > 0 ) {
@@ -221,7 +188,8 @@ const listupSongs = (library, isSuggestion, inputType) => {
       // When the list is for suggestion, get rid of custom input(criteria search)
       const title = document.querySelector("#sub-title");
       const inputs = document.querySelector(".criteria-wrapper");
-      const spacer = document.querySelector(".user-input");
+      const spacer = document.querySelector("#spacer");
+
       const newTitle = document.querySelector("#libState");
     
       spacer?.remove();
@@ -229,18 +197,9 @@ const listupSongs = (library, isSuggestion, inputType) => {
       inputs?.remove();
 
       newTitle.classList.remove("item-library");
-
-      // Add save results button
-      const button = document.createElement("button");
-      const buttonDiv = document.createElement("div");
-      //buttonDiv.setAttribute("class", "item-wraper"); 
-      button.setAttribute("type", "button");
-      button.setAttribute("onClick", "saveResults()");
-      button.innerText = "Save Results";
-      buttonDiv.appendChild(button);
-
-      contentTarget.parentNode.appendChild(buttonDiv);
-      //console.log(buttonDiv);
+      //let scoreButton = document.createElement("BUTTON");
+      //scoreButton.innerHTML = "TEST";
+      //document.getElementById("filterType").append(scoreButton);
     }
 }
 
@@ -258,7 +217,7 @@ function buttonSelected(selectedID){
     selectedSong = -1;
   }
   else{
-    // console.log(selectedID);
+    console.log(selectedID);
     document.getElementById('startButton').innerHTML = "Start";
     selectedSong = selectedID;
     document.getElementById(selectedSong).checked = true;
@@ -289,7 +248,7 @@ async function sendSelected(callback){
     while(contentTarget.firstChild) {
       contentTarget.removeChild(contentTarget.firstChild);
     }
-    libraryForSave = suggestion.results;
+
     listupSongs(suggestion.results, true, 1);
     sortByScore(document.getElementById("name"));
   }
@@ -318,7 +277,7 @@ async function sendSelected(callback){
     suggestion = new SuggestionWSong(song);
     await suggestion.beginSuggestion();
     // console.log(suggestion);
-    // console.log(suggestion);
+    console.log(suggestion);
     callback();
 
     document.querySelector(".item-title.item-library").innerHTML = "Suggestions";
@@ -344,6 +303,8 @@ document.querySelectorAll(".navButton")[0].addEventListener('click', () => {
 });
 
 // configuration nav
+
+
 lastCheckbox.firstElementChild.value = -2;
 lastCheckbox.firstElementChild.id = -2;
 //console.log(lastCheckbox.firstElementChild);
@@ -358,6 +319,7 @@ function toggleImage(value){
     return "../img/sort-down.png"; 
   }
 }
+
 
 function sortByBpm(element){
   var ascending = element.value; 
@@ -468,8 +430,7 @@ function prepareSortedLib(){
 }
 
 function prepareSortedResults(){
-  document.querySelector("#libState").innerText = "Suggestions";
-
+  document.querySelector(".item-title").innerHTML = "Suggestions";
   //document.querySelector(".button").removeChild(document.querySelector(".button").firstChild);
   while(contentTarget.firstChild) {
     contentTarget.removeChild(contentTarget.firstChild);
